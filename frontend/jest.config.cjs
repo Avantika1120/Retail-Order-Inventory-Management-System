764c6a3bc80f001af2b2ac2deca01338fe0fbe9a
Avantika Pandey <98182432+Avantika1120@users.noreply.github.com>
2026-09-09 12:46:11 -0400

    Configure ts-jest CommonJS transform

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,6 +1,20 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  moduleNameMapper: { '\\.(css|less|scss)$': '<rootDir>/src/styleMock.js' }
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: {
+        target: 'ES2020',
+        module: 'CommonJS',
+        moduleResolution: 'Node',
+        jsx: 'react-jsx',
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+        ignoreDeprecations: '6.0'
+      }
+    }]
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss)$': '<rootDir>/src/styleMock.js'
+  }
 }
